8468372a19265774de71aa8eda2368d46eda6050
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-01-24 16:53:37 +0100

    adjust plot recipe

--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -2,5 +2,5 @@
     seriestype --> :heatmap
     aspect_ratio --> 1
     grid --> false
-    val.xs, val.ys, @view replace(val.mat, val.nodata => NaN)[end:-1:1, :]
+    val.xs, val.ys, @view replace(val.mat, val.nodata => NaN)'[end:-1:1, :]
 end
