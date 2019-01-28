3448b5475aedaedf7c918e15a7edd621737bc966
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-01-29 00:18:02 +0100

    improve constructor

--- src/datatypes.jl
+++ src/datatypes.jl
@@ -3,17 +3,23 @@ struct VerySimpleRaster{T}
     nodata::T
     xs::StepRangeLen{Float64}
     ys::StepRangeLen{Float64}
-    proj::String
+    projection::String
     filename::String
 end
 
 VerySimpleRaster(mat::AbstractMatrix{T}, nodata, xll, yll, cellx, celly = cellx, proj = "", fname = "") where T =
     VerySimpleRaster{T}(mat, convert(T, nodata), range(xll, step = cellx, length = size(mat,1)), range(yll, step = celly, length = size(mat,2)), proj, abspath(fname))
 
+bbox(vsr::VerySimpleRaster) = (
+    xmin = minimum(vsr.xs),
+    xmax = maximum(vsr.xs) + step(vsr.xs),
+    ymin = minimum(vsr.ys),
+    ymax = maximum(vsr.ys) + step(vsr.ys))
+
 Base.show(io::IO, vsr::VerySimpleRaster{T}) where T = println(io,
 """
 VerySimpleRaster{$T} with $(length(vsr.ys)) rows and $(length(vsr.xs)) columns
 Resolution: $(round(step(vsr.xs), sigdigits = 5)), $(round(step(vsr.ys), sigdigits = 5))
-Extent: $(round(first(vsr.xs), sigdigits = 5)) to $(round(last(vsr.xs), sigdigits = 5)) longitude, $(round(first(vsr.ys), sigdigits = 5)) to $(round(last(vsr.ys), sigdigits = 5)) latitude
-Projection: $(vsr.proj)
+Extent: $(round(bbox(vsr).xmin, sigdigits = 5)) to $(round(bbox(vsr).xmax, sigdigits = 5)) longitude, $(round(bbox(vsr).ymin, sigdigits = 5)) to $(round(bbox(vsr).ymax, sigdigits = 5)) latitude
+Projection: $(vsr.projection)
 Source file: $(vsr.filename)""")
